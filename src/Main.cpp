c899cb53ae7cec9b079742dd0a51f7f3bb338c79
Jurij Kos <jurij.kos1996@live.com>
2020-01-11 19:39:50 +0100

    punjenje matrica

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,9 +3,13 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include "NeedlemanWunsch.cpp"
+#include <algorithm>
+//#include "NeedlemanWunsch.h"
 
 #define GET_INDEX(i, j, m) ((i) * (m) + (j)) 
+#define FROM_MM 1
+#define FROM_EX 2
+#define FROM_EY 3
 
 double emissionMatrix[5][5] = {
   { 0.0, 0.625, 0.375, 0.0, 0.0 },
@@ -43,6 +47,21 @@ char** allocateChar2D(int n, int m) {
   return matrix;
 }
 
+int getIndexOfBase(char c) {
+  switch (c) {
+    case '-':
+      return 0;
+    case 'A':
+      return 1;
+    case 'C':
+      return 2;
+    case 'G':
+      return 3;
+    case 'T':
+      return 4;  
+  }
+  return -1;
+}
 
 void viterbi() {
   
@@ -77,8 +96,8 @@ void viterbi() {
 
   
   double **withMM = allocateDouble2D(n + 1, m + 1);
-  double **emitX = allocateDouble2D(n + 1, m + 1);
-  double **emitY = allocateDouble2D(n + 1, m + 1);
+  double **withEmitX = allocateDouble2D(n + 1, m + 1);
+  double **withEmitY = allocateDouble2D(n + 1, m + 1);
 
   char **traceMM = allocateChar2D(n + 1, m + 1);
   char **traceX = allocateChar2D(n + 1, m + 1);
@@ -89,14 +108,64 @@ void viterbi() {
 
   withMM[0][0] = 1; 
 
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+        char x = firstGene[i - 1];
+        char y = secondGene[j - 1];
+        double pxy = emissionMatrix[getIndexOfBase(x)][getIndexOfBase(y)];
+        double qx = emissionMatrix[getIndexOfBase(x)][0];
+        double qy = emissionMatrix[0][getIndexOfBase(y)];
+        std::cout << pxy + qx + qy << std::endl;
+        
+        double wasMM = transmissionMatrix[3][3] * withMM[i - 1][j - 1];
+        double wasEmitX = transmissionMatrix[2][3] * withEmitX[i - 1][j - 1];
+        double wasEmitY = transmissionMatrix[1][3] * withEmitY[i - 1][j - 1];
 
+        if (wasMM > wasEmitX && wasMM > wasEmitY) {
+          withMM[i][j] = pxy *wasMM;
+          traceMM[i][j] =  FROM_MM; 
+        } else if (wasEmitX > wasMM && wasEmitX > wasEmitY) {
+          withMM[i][j] = pxy * wasEmitX;
+          traceMM[i][j] =  FROM_EX;
+        } else {
+          withMM[i][j] = pxy * wasEmitY;
+          traceMM[i][j] = FROM_EY;
+        }
+
+        //emit in x
+        wasMM = transmissionMatrix[3][2] * withMM[i - 1][j];
+        wasEmitX = transmissionMatrix[2][2] * withEmitX[i - 1][j];
+        
+        if (wasMM > wasEmitX) {
+          withEmitX[i][j] = qx *wasMM;
+          traceX[i][j] =  FROM_MM; 
+        } else {
+          withEmitX[i][j] = qx * wasEmitX;
+          traceX[i][j] = FROM_EX;
+        }
+
+        
+        //emit in y
+        wasMM = transmissionMatrix[3][1] * withMM[i][j - 1];
+        wasEmitY = transmissionMatrix[1][1] * withEmitY[i][j - 1];
+        
+        if (wasMM > wasEmitY) {
+          withEmitY[i][j] = qy * wasMM;
+          traceY[i][j] =  FROM_MM; 
+        } else {
+          withEmitY[i][j] = qy * wasEmitY;
+          traceY[i][j] = FROM_EY;
+        }
+    }
+  }
   
 }
 
 int main(void)
 {
-  viterbi();
+  //viterbi();
   //new NeedlemanWunsch();
+  std::cout << std::max({1, 3, 4}) << std::endl;
 
   return 0;
 }
